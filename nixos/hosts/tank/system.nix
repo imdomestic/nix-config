6a8a0c4cf8d9da5ba49c1cf42ebde470a3158900
Hank Hogan <ysh2291939848@outlook.com>
2026-04-11 15:32:49 +0800

    update

--- nixos/hosts/tank/system.nix
+++ nixos/hosts/tank/system.nix
@@ -593,7 +593,6 @@ in {
     cachix
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    neovim
     git
     gcc
     wqy_microhei
