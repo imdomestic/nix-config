3e6c05249a2394c6e4d3f68317ec363a45d78aba
Hank Hogan <ysh2291939848@outlook.com>
2026-09-11 21:24:05 +1000

    fix(b650): keep headless Mutter off NVIDIA render nodes

--- nixos/hosts/b650/desktop.nix
+++ nixos/hosts/b650/desktop.nix
@@ -15,8 +15,8 @@
 
     # Mutter must leave the inference GPU alone, including in the GDM greeter.
     udev.extraRules = ''
-      SUBSYSTEM=="drm", KERNEL=="card[0-9]*", ATTRS{vendor}=="0x1002", ATTRS{device}=="0x13c0", TAG+="mutter-device-preferred-primary"
-      SUBSYSTEM=="drm", KERNEL=="card[0-9]*", ATTRS{vendor}=="0x10de", ATTRS{device}=="0x2b8c", TAG+="mutter-device-ignore"
+      SUBSYSTEM=="drm", KERNEL=="card[0-9]*|renderD[0-9]*", ATTRS{vendor}=="0x1002", ATTRS{device}=="0x13c0", TAG+="mutter-device-preferred-primary"
+      SUBSYSTEM=="drm", KERNEL=="card[0-9]*|renderD[0-9]*", ATTRS{vendor}=="0x10de", ATTRS{device}=="0x2b8c", TAG+="mutter-device-ignore"
     '';
   };
 
